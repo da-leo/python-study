bb44c882be3fcf98f2d08b6b729160cc67cec294
qinpei <qinpei@zingfront.com>
2020-10-14 09:40:55 +0800

    装饰器

---
+++ decorator/a_new_decorator.py
@@ -0,0 +1,28 @@
+from functools import wraps
+
+
+def a_new_decorator(func):
+    @wraps(func)
+    def wrapTheFunction():
+        print("i am doing some work before")
+        func()
+        print("i am doing some work after")
+    return wrapTheFunction
+
+def test_decorator_func():
+    print('testing ...')
+
+
+test_decorator_func()
+a = a_new_decorator(test_decorator_func)
+a()
+
+
+# 进阶
+@a_new_decorator
+def test_decorator_func2():
+    print('testing2 ...')
+
+
+test_decorator_func2()
+
